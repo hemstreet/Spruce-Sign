e2fdebd57f6891f618dc755708c30dc392e9b784
Hemstreet <jonhemstreet@gmail.com>
2015-06-03 03:00:08 -0600

    testing default connect

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var sign = {
     init: function () {
 
         console.log('Connecting', this.totalLeds);
-        ws2801.connect(this.totalLeds, '/dev/spidev0.0');
+        ws2801.connect(this.totalLeds);
 
     },
     //Create a pattern that is every other one
