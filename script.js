930997bcd8c801368f737b13afe3dd35be303cf5
Hemstreet <jonhemstreet@gmail.com>
2015-06-03 20:27:23 -0600

    adding light

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var ws2801 = require('rpi-ws2801'),
 
 var sign = {
 
-    totalLeds: 50,
+    totalLeds: 75,
     loops: 5, // number of times to run the light loops i.e. for appointments booked
     currentLoop: 0,
 
